06e99f00ff37399c86ba7bc7554f4555044b51c3
Brandon J. Mondoy M. <brandon.mondoy@stud.unifi.it>
2025-09-05 19:01:25 +0200

    Create list.s
    Added the file with the general structure of the program.
    That one file will be the only in the project.

---
+++ list.s
@@ -0,0 +1,31 @@
+.data
+
+testProf1: .string "ADD(1) ~ ADD(a) ~ ADD(a) ~ ADD(B) ~ ADD(;) ~ ADD(9) ~SORT~PRINT~DEL(b) ~DEL(B)~PRI~REV~PRINT"
+testProf2: .string "ADD(1) ~ ADD(a) ~ add(B) ~ ADD(B) ~ ADD ~ ADD(9) ~PRINT~SORT(a)~PRINT~DEL(bb) ~DEL(B) ~PRINT~REV~PRINT"
+testAdd: .string ""
+
+.text
+
+main:
+    
+    
+    # Terminate program 
+
+add:
+    
+    
+del:
+    
+    
+print:
+    
+sort:
+    
+rev:
+    
+sdx:
+    
+ssx:
+        
+test: 
+
